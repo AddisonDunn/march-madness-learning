f2da26f12197b3faba4e2ef605ec7bce698291a6
Addison Dunn <DunnAddisonW@gmail.com>
2016-07-25 23:34:02 -0400

    Added new main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import MySQLdb;
+from sqlalchemy import *;
+from sqlalchemy.ext.automap import automap_base;
+from sqlalchemy.orm import Session, relationship;
+from sqlalchemy.ext.declarative import declarative_base;
+from loadData import Team, SeasonResult, TourneyResult;
+
+Base = automap_base()
+
+#----------------------------------------------------------------------------
+
+def mainAlgo(session):
+
+	# ITERATE THROUGH YEARS:
+	currentYear = 2014
+	tourneyResultList = [instance for instance in session.query(TourneyResult).filter(TourneyResult.season == currentYear)]
+	teamList = [result.name for result in tourneyResultList]
+	
+
+#----------------------------------------------------------------------------
+
+#MAIN
+
+username = "root"
+pword = "password"
+name = "madnessdata"
+
+# Initialize the database :: Connection & Metadata retrieval
+engine = create_engine('mysql+mysqldb://' + username + ':' + pword + '@localhost/' + name)
+Base.prepare(engine, reflect=True)
+
+# SqlAlchemy :: Starts a session
+mainSession = Session(engine)
+
+Base.metadata.create_all(engine)
+
+
+mainAlgo(mainSession)
